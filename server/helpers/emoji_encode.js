e2c74fa6f4c101c73ed1ae0c5e929b7cb7ba5e99
carterbrooks <carter.brooks91@gmail.com>
2023-08-25 16:09:28 -0500

    insert line to check if text is missing when it's sent to the emoji encoder create tests for emoji encoder

--- server/helpers/emoji_encode.js
+++ server/helpers/emoji_encode.js
@@ -1,10 +1,15 @@
 import emojiRegex from 'emoji-regex';
 
 const encodeEmojis = (text) => {
+  if (!text) {
+    return Promise.reject(new Error('Missing text for emoji matching'))
+  }
+
   const emojiMatches = text.match(emojiRegex());
   if (!emojiMatches) {
     return text;
   }
+
   let encodedText = text;
   emojiMatches.forEach((emoji) => {
     encodedText = encodedText.replace(emoji, encodeURIComponent(emoji));
