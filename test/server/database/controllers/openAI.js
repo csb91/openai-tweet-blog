0fbc548ac6f223c86b68911d0091be754781b434
carterbrooks <carter.brooks91@gmail.com>
2023-08-28 16:19:10 -0500

    write tests for generateTweetsWithAPI

--- test/server/database/controllers/openAI.js
+++ test/server/database/controllers/openAI.js
@@ -12,13 +12,20 @@ import { Configuration, OpenAIApi } from 'openai';
 import { validateInput, generateTweetsWithAPI, saveTweetsToDb, generateTweets } from '../../../../server/database/controllers/openAI.js';
 import Tweet from '../../../../server/database/models/tweets.js';
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 describe('openAI Controller', () => {
   let sandbox;
   let request;
   let response;
   let sampleTweet;
+  let createCompletionStub;
   let findStub;
   let createStub;
+  let errorStub = new Error('Mock error');
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
@@ -46,6 +53,8 @@ describe('openAI Controller', () => {
       created_date: new Date(2023, 7, 26, 12, 0, 0, 0),
       tweet_date: ''
     };
+
+    createCompletionStub = sandbox.stub(OpenAIApi.prototype, 'createCompletion').resolves({data: {choices: [{text: ' 1. This is a tweet\n2. This is another tweet\n3. One more tweet'}]}});
   });
 
   afterEach(() => {
@@ -55,7 +64,7 @@ describe('openAI Controller', () => {
 
   context('validateInput', () => {
     it('should check for an error to eventually be thrown when the model type is missing', () => {
-      request.body.model = '';
+      request.body.model = ''
 
       return expect(validateInput(request)).to.be.eventually.rejectedWith('Missing required input parameter');
     });
@@ -70,23 +79,74 @@ describe('openAI Controller', () => {
       request.body.numberTweets = '';
 
       return expect(validateInput(request)).to.be.eventually.rejectedWith('Missing required input parameter');
-    })
+    });
 
     it('should check for an error to eventually be throw when the temperature is missing', () => {
       request.body.temperature = '';
 
       return expect(validateInput(request)).to.be.eventually.rejectedWith('Missing required input parameter');
-    })
+    });
 
     it('should check for an error to eventually be throw when the max tokens are missing', () => {
       request.body.max_tokens = '';
 
       return expect(validateInput(request)).to.be.eventually.rejectedWith('Missing required input parameter');
-    })
+    });
 
     it('should resolve eventually when all the required input parameters are present', () => {
 
       return expect(validateInput(request)).to.be.fulfilled;
-    })
+    });
   });
+
+  context('generateTweetsWithAPI', () => {
+    it('should return error when there is an issue with createCompletion', () => {
+      createCompletionStub.rejects(errorStub);
+
+      return expect(generateTweetsWithAPI(request)).to.be.eventually.rejectedWith('An error occurred while generating tweets with openAI API');
+    });
+
+    it('should return generated tweets correctly split into an array for each tweet when multiple tweets generated', async () => {
+      let results = await generateTweetsWithAPI(request);
+
+      expect(results).to.deep.equal(['1. This is a tweet', '2. This is another tweet', '3. One more tweet']);
+      expect(createCompletionStub).to.have.been.calledOnceWith({
+        model: 'text-davinci-003',
+        prompt:'\n  Generate 3 tweets using this prompt: create tweets about react,\n  Responses should always be numbered followed by a period and a single space followed by the tweet.\n  Responses should always be numbered followed by a period and a single space followed by the tweet.\n  ',
+        temperature: 1,
+        max_tokens: 500
+      });
+    });
+
+    it('should return generated tweet correctly split into array when there is only a single tweet generated', async () => {
+      request.body.numberTweets = 1;
+      createCompletionStub.resolves({data: {choices: [{text: ' 1. This is the only tweet'}]}});
+
+      let results = await generateTweetsWithAPI(request);
+
+      expect(results).to.deep.equal(['1. This is the only tweet']);
+      expect(createCompletionStub).to.have.been.calledOnceWith({
+        model: 'text-davinci-003',
+        prompt:'\n  Generate 1 tweets using this prompt: create tweets about react,\n  Responses should always be numbered followed by a period and a single space followed by the tweet.\n  Responses should always be numbered followed by a period and a single space followed by the tweet.\n  ',
+        temperature: 1,
+        max_tokens: 500
+      });
+    });
+
+    it('should return an array with an empty string if no tweets are generated', async () => {
+      request.body.numberTweets = 0;
+      createCompletionStub.resolves({data: {choices: [{text: ''}]}});
+
+      let results = await generateTweetsWithAPI(request);
+
+      expect(results).to.deep.equal([''])
+      expect(createCompletionStub).to.have.been.calledOnceWith({
+        model: 'text-davinci-003',
+        prompt:'\n  Generate 0 tweets using this prompt: create tweets about react,\n  Responses should always be numbered followed by a period and a single space followed by the tweet.\n  Responses should always be numbered followed by a period and a single space followed by the tweet.\n  ',
+        temperature: 1,
+        max_tokens: 500
+      });
+    });
+
+  })
 });
